cc0324fff8ab7f4d884321ae6e0911d7e930ef2a
paul correa <85908848+paulcj20@users.noreply.github.com>
2022-09-04 23:53:02 -0300

    commit

--- js/sobreMi.js
+++ js/sobreMi.js
@@ -1,5 +1,12 @@
+//sobre mi
 const tazas = document.querySelector('.contador-tazas');
 const lineas = document.querySelector('.contador-lineas');
+// index
+const titulo = document.querySelector(h1);
+const subtitulo = document.querySelector(h2);
+const ubicacion = document.querySelector('.ubicacion');
+const boton = document.querySelector('.boton');
+const botones2 = document.querySelector('.boton-secundario');
 
 let agregar1 = function() {
     let valorTazas = parseInt(tazas) ;
@@ -23,4 +30,10 @@ let agregar2 = function() {
 
 document.addEventListener('DOMContentLoaded', () => {
     //setTimeout(agregar1, 3000)
+    setTimeout(titulo.removeAttribute('hidden'), 100);
+    setTimeout(subtitulo.removeAttribute('hidden'), 200);
+    setTimeout(ubicacion.removeAttribute('hidden'), 300);
+    setTimeout(boton.removeAttribute('hidden'), 400);
+    setTimeout(botones2.removeAttribute('hidden'), 500);
+    
 })
